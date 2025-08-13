b1133990f6ac42e0f677f3bc489d769239ed7e36
Waseem <98299006+waseemR02@users.noreply.github.com>
2025-08-13 16:35:42 +0530

    add fzf-lua for searching files faster in bigger codebases

--- init.lua
+++ init.lua
@@ -963,7 +963,17 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
-
+  {
+    'ibhagwan/fzf-lua',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('fzf-lua').setup {}
+    end,
+    keys = {
+      { '<leader>szf', '<cmd>FzfLua files<cr>', desc = 'Find Files with fzf' },
+    },
+  },
+    
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
